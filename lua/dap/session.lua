09d0e3ede9e79eacd871519d860046172215564f
nagaohiroki <testplayers0226@gmail.com>
2024-11-11 23:27:21 +0900

    fix vstuc error

--- lua/dap/session.lua
+++ lua/dap/session.lua
@@ -1007,7 +1007,7 @@ function Session:set_exception_breakpoints(filters, exceptionOptions, on_done)
   --- exceptionOptions: exceptionOptions?: ExceptionOptions[] (https://microsoft.github.io/debug-adapter-protocol/specification#Types_ExceptionOptions)
   self:request(
     'setExceptionBreakpoints',
-    { filters = filters, exceptionOptions = exceptionOptions },
+    { filters = filters, exceptionOptions = exceptionOptions, filterOptions = {} },
     function(err, _)
       if err then
         utils.notify('Error setting exception breakpoints: ' .. utils.fmt_error(err), vim.log.levels.ERROR)
